7ae6b5f0d535c27e0e525508b91b917339c3d1c3
Nik Semenov <nikita.semenov@six.de>
2021-09-01 16:00:00 +0200

    session handling, can now use tm.sh to reopen my tab set

--- tm.sh
+++ tm.sh
@@ -4,21 +4,17 @@ session=$(whoami)
 
 # Check if the session exists, discarding output
 # We can check $? for the exit status (zero for success, non-zero for failure)
-tmux has-session -t $session 2>/dev/null
 
-if [ $? != 0 ]; then
+tmux new-session -A -s $session -n mydotfiles 'cd ~/mydotfiles; git pull && vim .; /usr/bin/zsh '
 
-    tmux new-session -d -s $session -n mydotfiles 'cd ~/mydotfiles; git pull && vim .; /usr/bin/zsh '
+#tmux new-window -n nadmozg -t nikita: 'cd ~/nadmozg.wiki; git pull && vim .; /usr/bin/zsh '
+#tmux new-window -n nadmozg -t $session: 'jp' 
 
-    #tmux new-window -n nadmozg -t nikita: 'cd ~/nadmozg.wiki; git pull && vim .; /usr/bin/zsh '
-    #tmux new-window -n nadmozg -t $session: 'jp' 
+# hosts are configured in ~/.ssh/config
+sed -rn "s/^\s*Host\s+(.*)\s*/\1/ip" ~/.ssh/config | while read host; do tmux new-window -n ssh_$host -t $session: "ssh $host -t \"export HISTFILE=~/.bash_history_$session; tmux  -L $session new-session -A -s $session \""; done
 
-    # hosts are configured in ~/.ssh/config
-    sed -rn "s/^\s*Host\s+(.*)\s*/\1/ip" ~/.ssh/config | while read host; do tmux new-window -n ssh_$host -t $session: "ssh $host -t \"export HISTFILE=~/.bash_history_$session; tmux  -L $session new-session -A -s $session \""; done
+tmux new-window -n python -t $session: 'cd; python3' 
 
-    tmux new-window -n python -t $session: 'python3' 
-
-fi
 
 # Attach to created session
-tmux attach-session -t $session
+#tmux attach-session -t $session
