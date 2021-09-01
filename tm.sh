f1dbe361bb74caa4b58452cecbe0093921883aad
Nik Semenov <nikita.semenov@six.de>
2021-09-01 16:35:14 +0200

    only 2 ssh sessions; script does work with no session & with existing session

--- tm.sh
+++ tm.sh
@@ -5,16 +5,21 @@ session=$(whoami)
 # Check if the session exists, discarding output
 # We can check $? for the exit status (zero for success, non-zero for failure)
 
-tmux new-session -A -s $session -n mydotfiles 'cd ~/mydotfiles; git pull && vim .; /usr/bin/zsh '
+tmux new-session -d -s $session -n mydotfiles 'cd ~/mydotfiles; git pull && vim .; /usr/bin/zsh '
 
 #tmux new-window -n nadmozg -t nikita: 'cd ~/nadmozg.wiki; git pull && vim .; /usr/bin/zsh '
 #tmux new-window -n nadmozg -t $session: 'jp' 
 
 # hosts are configured in ~/.ssh/config
-sed -rn "s/^\s*Host\s+(.*)\s*/\1/ip" ~/.ssh/config | while read host; do tmux new-window -n ssh_$host -t $session: "ssh $host -t \"export HISTFILE=~/.bash_history_$session; tmux  -L $session new-session -A -s $session \""; done
+sed -rn "s/^\s*Host\s+(.*)\s*/\1/ip" ~/.ssh/config | while read host; do 
+    echo $host;
+    tmux new-window -n ssh_$host -t $session: "ssh $host -t \"export HISTFILE=~/.bash_history_$session; tmux  -L $session new-session -A -s $session \""
+    ((c++)) && ((c==2)) && break
+done
 
 tmux new-window -n python -t $session: 'cd; python3' 
+#tmux new-window -n mydotfiles 'cd ~/mydotfiles; git pull && vim .; /usr/bin/zsh '
 
 
 # Attach to created session
-#tmux attach-session -t $session
+tmux attach-session -t $session
