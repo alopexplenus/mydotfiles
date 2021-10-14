41a16d6ef13f010179ec9387a161a75d5442ec97
Nik Semenov <nikita.semenov@six.de>
2021-10-14 14:53:17 +0200

    updated tm script

--- tm.sh
+++ tm.sh
@@ -2,23 +2,34 @@
 
 session=$(whoami)
 
+
+
+monthly_note=$(date +"%Y-%m.md")
+
+
 # Check if the session exists, discarding output
 # We can check $? for the exit status (zero for success, non-zero for failure)
+#
 
-tmux new-session -d -s $session -n mydotfiles 'cd ~/mydotfiles; git pull && vim .; /usr/bin/zsh '
+tmux has-session -t $session ||  tmux new-session -d -s $session;
 
-tmux new-window -n notes -t $session: 'cd ~/Sync/obsidian/; vim tickets/tickets.md Nadmozg/index.md -p ' 
+tmux neww  -k -n tickets -t $session: 'cd ~/Sync/obsidian/; vim tickets/tickets.md ;  /usr/bin/zsh ' 
+tmux neww  -k -n notes -t $session: "cd ~/Sync/obsidian/; vim Nadmozg/index.md Nadmozg/$monthly_note -p;  /usr/bin/zsh " 
 
-# hosts are configured in ~/.ssh/config
+ # hosts are configured in ~/.ssh/config
 sed -rn "s/^\s*Host\s+(.*)\s*/\1/ip" ~/.ssh/config | while read host; do 
     echo $host;
-    tmux new-window -n ssh_$host -t $session: "ssh $host -t \"export HISTFILE=~/.bash_history_$session; tmux  -L $session new-session -A -s $session \""
+    tmux neww  -k -n ssh_$host -t $session: "ssh $host -t \"export HISTFILE=~/.bash_history_$session; tmux  -L $session new-session -A -s $session \""
     ((c++)) && ((c==2)) && break
 done
 
-tmux new-window -n python -t $session: 'cd; python3' 
-#tmux new-window -n mydotfiles 'cd ~/mydotfiles; git pull && vim .; /usr/bin/zsh '
 
+tmux neww -k -n python -t $session: 'cd; python3' 
 
-# Attach to created session
-tmux attach-session -t $session
+tmux attach -t $session
+
+# todo: check if window exists, with output from list-windows
+#➜  ~ tmux list-windows -t nik
+#1: ~#- (1 panes) [80x24] [layout b262,80x24,0,0,5] @5
+#2: python* (1 panes) [80x24] [layout b263,80x24,0,0,6] @6 (active)
+
