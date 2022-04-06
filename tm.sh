4b0af1456c0e7cf700832352680783666d04f213
Nik Semenov <n.semenov@stroeermediabrands.de>
2022-04-06 10:08:05 +0200

    no named history file for ssh sessions, disabled python tab

--- tm.sh
+++ tm.sh
@@ -2,6 +2,7 @@
 
 session=$(whoami)
 monthly_note=$(date +"%Y-%m.md")
+weekly_note=$(date +"%Y-KW%V.md")
 
 # arguments are session name and window name
 window_exists(){
@@ -17,17 +18,20 @@ window_exists(){
 tmux has-session -t $session ||  tmux new-session -d -s $session;
 
 # window_exists $session "tickets" || tmux neww  -k -n tickets -t $session: 'cd ~/projects/tickettool/tickets/; vim tickets.md ;  /usr/bin/zsh ' 
-window_exists $session "notes" || tmux neww  -k -n notes -t $session: "cd ~/Sync/obsidian/Nadmozg/; vim index.md $monthly_note -p;  /usr/bin/zsh " 
+window_exists $session "notes" || tmux neww  -k -n notes -t $session: "cd ~/notes; vim $weekly_note ;  /usr/bin/zsh " 
 
  # hosts are configured in ~/.ssh/config
 sed -rn "s/^\s*Host\s+(.*)\s*/\1/ip" ~/.ssh/config | while read host; do 
     #echo "ssh_$host";
-    window_exists $session "ssh_$host" || tmux neww  -k -n ssh_$host -t $session: "ssh $host -t \"export HISTFILE=~/.bash_history_$session; tmux  -L $session new-session -A -s $session \""
+    # old version with custom history file no longer needed
+    #window_exists $session "ssh_$host" || tmux neww  -k -n ssh_$host -t $session: "ssh $host -t \"export HISTFILE=~/.bash_history_$session; tmux  -L $session new-session -A -s $session \""
+    # new version with tmux session
+    window_exists $session "ssh_$host" || tmux neww  -k -n ssh_$host -t $session: "ssh $host -t \"tmux  -L $session new-session -A -s $session \""
     ((c++)) && ((c==2)) && break
 done
 
 
-window_exists $session "python" || tmux neww -k -n python -t $session: 'cd; python3' 
+#window_exists $session "python" || tmux neww -k -n python -t $session: 'cd; python3' 
 
 tmux attach -t $session
 
