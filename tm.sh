d1d2dba4e98f9193b925049672756b9ecea3772b
nik <n.semenov@stroeermediabtands.de>
2022-10-05 16:42:26 +0200

    clean up todo notes
    after putting done taskt into previous week note,
    delete them from todos

--- tm.sh
+++ tm.sh
@@ -10,8 +10,12 @@ if [[ ! -f ~/notes/$weekly_note ]]; then
     echo $(date +"# %d.%m\n") >> ~/notes/$weekly_note
 
     echo "\n\n# What have I done?!\n" >> ~/notes/$previous_weekly_note
+
     cat ~/notes/todos.md | grep "\[[xX]\]" >> ~/notes/$previous_weekly_note
+    sed -i '/^\- \[[xX]\].*/d' ~/notes/todos.md
+
     cat ~/notes/f/todos.md | grep "\[[xX]\]" >> ~/notes/$previous_weekly_note
+    sed -i '/^\- \[[xX]\].*/d' ~/notes/f/todos.md
 
 fi
 
