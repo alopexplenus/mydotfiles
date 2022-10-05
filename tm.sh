d6347b7f8474b40d1c8740d05582ffe1677fa4e5
nik <n.semenov@stroeermediabtands.de>
2022-10-05 15:27:05 +0200

    pm alias with venv, header for new weekly note

--- tm.sh
+++ tm.sh
@@ -2,12 +2,14 @@
 
 session=$(whoami)
 weekly_note=$(date +"%Y-KW%V.md")
+weekly_note_heading=$(date +"# KW%V\n")
 previous_weekly_note=$(date +"%Y-KW%V.md" -d "7 days ago")
 
 if [[ ! -f ~/notes/$weekly_note ]]; then
-    echo $(date +"# %d.%m\n") > ~/notes/$weekly_note
+    echo "$weekly_note_heading" > ~/notes/$weekly_note
+    echo $(date +"# %d.%m\n") >> ~/notes/$weekly_note
 
-    echo "\n\n# What have I done?!\n\n" > ~/notes/$previous_weekly_note
+    echo "\n\n# What have I done?!\n" >> ~/notes/$previous_weekly_note
     cat ~/notes/todos.md | grep "\[[xX]\]" >> ~/notes/$previous_weekly_note
     cat ~/notes/f/todos.md | grep "\[[xX]\]" >> ~/notes/$previous_weekly_note
 
